8a34880512ba5f6e78caa03960fa6c888cc8e1e8
Laura <lhernan53112@gmail.com>
2020-05-20 15:36:07 -0400

    Created html routes for index and notes.html. Added html routes to server.js.Added static files from public directory to server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var app = express();
+var path= require("path");
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-app.get('/',function(req, res){
-    res.send("server works!");
-})
+//Routes
+require("./routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
